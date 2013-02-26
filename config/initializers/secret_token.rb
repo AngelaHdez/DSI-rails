9833b2562d78dc06bd7d8cc85ac5ae991992b61b
angela <angela@ubuntu.(none)>
2013-02-26 13:54:39 +0000

    subiendo heroku

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DSIGit::Application.config.secret_token = '26dcded303e1b6d077f04e41d8212cc36e7fefa2f71ba97007e4fcc99428b697a8ad3eeb04ca34d3d6bc2fe3a2bd787cd4fdf73be995fa4d1f5352b026bf6e04'
